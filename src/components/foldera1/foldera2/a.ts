80f9eb6946a7e58d85ed9f446ddcd549d3a9dfcd
Daniel Qin <xin.qin@qinx.org>
2019-06-17 18:45:43 +0800

    init

---
+++ src/components/foldera1/foldera2/a.ts
@@ -0,0 +1,11 @@
+export class A1 {
+  test() {
+    console.log('A1');
+  }
+}
+
+export class A2 {
+  test() {
+    console.log('A2');
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import * as X from './components';
+export { X };
+export * from './components';
