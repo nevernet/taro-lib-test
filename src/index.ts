efe3f9f5290f6c91c48b5ccb7daae0c7390bfe99
Daniel Qin <xin.qin@qinx.org>
2019-06-17 18:51:42 +0800

    added dist

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import * as X from './components';
-export { X };
-export * from './components';
+export { X }; // 这里是必须的，否则components目录不会实际导出
+
+// 实际项目里面，导出的类有好几百个， 太多了，无法单个导出
+export * from './components'; // 单纯这样，无法导出整个components目录
